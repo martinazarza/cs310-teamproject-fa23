daed8548e087bad6682d3b4bffa72a0c282b087e
Xavier Bausley <xbausley@stu.jsu.edu>
2023-10-13 11:44:43 -0500

    Added necessary getter methods for all of the data attributes

--- src/edu/jsu/mcis/cs310/tas_fa23/Employee.java
+++ src/edu/jsu/mcis/cs310/tas_fa23/Employee.java
@@ -10,15 +10,43 @@ package edu.jsu.mcis.cs310.tas_fa23;
  */
 import java.time.*;
 public class Employee {
-    private long id;
-    private String firstname;
-    private String middlename;
-    private String lastname;
-    private LocalDateTime localtime;
+    private long ID;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private LocalDateTime localDateTime;
     private Badge badge;
     private Department department;
     private Shift shift;
     private EmployeeType employeeType;
+    
+    public long getID(){
+        return ID;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getMiddleName(){
+       return middleName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public LocalDateTime getLocalDateTime(){
+        return localDateTime;
+    }
+    public Badge getBadge(){
+        return badge;
+    }
+    public Department getDepartment(){
+        return department;
+    }
+    public Shift getShift(){
+        return shift;
+    }
+    public EmployeeType getEmployeeType(){
+        return employeeType;
+    }
     
     
 }
