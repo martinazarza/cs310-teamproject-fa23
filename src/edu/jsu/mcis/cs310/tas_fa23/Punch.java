eb470dbe0e6ec4811c022d49adb3ff1c83db2e3f
Jalen Momon <Jmomon@stu.jsu.edu>
2023-10-20 11:21:10 -0500

    Added Punch Class

--- src/edu/jsu/mcis/cs310/tas_fa23/Punch.java
+++ src/edu/jsu/mcis/cs310/tas_fa23/Punch.java
@@ -1,25 +1,97 @@
 package edu.jsu.mcis.cs310.tas_fa23;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Punch {
     private int id;
     private int terminalId;
     private int badgeId;
     private long originalTimeStamp;
-    private long adjustedTimeStamp;
+    private LocalDateTime adjustedTimeStamp;
     private PunchAdjustmentType adjustmentType;
 
     public Punch(int terminalId, Badge badge, EventType punchType) {
         this.terminalId = terminalId;
         this.badgeId = badge.getId();
         this.originalTimeStamp = System.currentTimeMillis();
-        this.adjustedTimeStamp = this.originalTimeStamp;
+        this.adjustedTimeStamp = LocalDateTime.ofEpochSecond(this.originalTimeStamp / 1000, 0, null);
         this.adjustmentType = PunchAdjustmentType.NONE;
     }
 
     public Punch(int id, int terminalId, int badgeId, long originalTimeStamp, int punchTypeId) {
+        this.id = id;
+        this.terminalId = terminalId;
+        this.badgeId = badgeId;
+        this.originalTimeStamp = originalTimeStamp;
+        this.adjustedTimeStamp = LocalDateTime.ofEpochSecond(this.originalTimeStamp / 1000, 0, null);
+        this.adjustmentType = PunchAdjustmentType.NONE;
+    }
+
+    public Punch(int terminalId, int badgeId, long originalTimeStamp, int punchTypeId) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void adjust(Shift s) {
+        LocalTime shiftStart = s.getShiftStart();
+        LocalTime shiftStop = s.getShiftStop();
+        LocalTime lunchStart = s.getLunchStart();
+        LocalTime lunchStop = s.getLunchStop();
+        int roundInterval = s.getRoundInterval();
+        int gracePeriod = s.getGracePeriod();
+        int dockPenalty = s.getDockPenalty();
+
+        LocalTime originalTime = adjustedTimeStamp.toLocalTime();
+        PunchAdjustmentType AdjustmentType = PunchAdjustmentType.NONE;
+
+        // Adjust based on the shift parameters
+        if (originalTime.isBefore(shiftStart)) {
+            if (shiftStart.minusMinutes(roundInterval).isBefore(originalTime)) {
+                adjustedTimeStamp = adjustedTimeStamp.with(shiftStart);
+                AdjustmentType = PunchAdjustmentType.SHIFT_START;
+            } else {
+                adjustedTimeStamp = adjustedTimeStamp.plusMinutes((roundInterval - originalTime.getMinute() % roundInterval) % roundInterval)
+                        .withSecond(0);
+                AdjustmentType = PunchAdjustmentType.INTERVAL_ROUND;
+            }
+        } else if (originalTime.isAfter(shiftStop)) {
+            if (originalTime.isBefore(shiftStop.plusMinutes(roundInterval))) {
+                adjustedTimeStamp = adjustedTimeStamp.with(shiftStop);
+                AdjustmentType = PunchAdjustmentType.SHIFT_STOP;
+            } else {
+                adjustedTimeStamp = adjustedTimeStamp.minusMinutes(originalTime.getMinute() % roundInterval)
+                        .withSecond(0);
+                AdjustmentType = PunchAdjustmentType.INTERVAL_ROUND;
+            }
+        } else if (originalTime.isAfter(shiftStart) && originalTime.isBefore(lunchStart)) {
+            if (originalTime.isAfter(shiftStart.plusMinutes(gracePeriod))) {
+                adjustedTimeStamp = adjustedTimeStamp.with(shiftStart);
+                AdjustmentType = PunchAdjustmentType.GRACE_PERIOD;
+            }
+        } else if (originalTime.isAfter(lunchStop) && originalTime.isBefore(shiftStop)) {
+            if (originalTime.isBefore(shiftStop.minusMinutes(gracePeriod))) {
+                adjustedTimeStamp = adjustedTimeStamp.with(shiftStop);
+                AdjustmentType = PunchAdjustmentType.GRACE_PERIOD;
+            }
+        } else if (originalTime.isAfter(lunchStart) && originalTime.isBefore(lunchStop)) {
+            if (lunchStart.minusMinutes(roundInterval).isBefore(originalTime)) {
+                adjustedTimeStamp = adjustedTimeStamp.with(lunchStart);
+                AdjustmentType = PunchAdjustmentType.LUNCH_START;
+            } else {
+                adjustedTimeStamp = adjustedTimeStamp.plusMinutes((roundInterval - originalTime.getMinute() % roundInterval) % roundInterval)
+                        .withSecond(0);
+                AdjustmentType = PunchAdjustmentType.INTERVAL_ROUND;
+            }
+        } else if (originalTime.isBefore(lunchStart) || originalTime.isAfter(lunchStop)) {
+            if (originalTime.isAfter(shiftStart.plusMinutes(gracePeriod)) && originalTime.isBefore(shiftStop.minusMinutes(gracePeriod))) {
+                if (originalTime.isAfter(shiftStart.plusMinutes(gracePeriod + dockPenalty))) {
+                    adjustedTimeStamp = adjustedTimeStamp.with(shiftStart).plusMinutes(dockPenalty);
+                    AdjustmentType = PunchAdjustmentType.DOCK_PENALTY;
+                }
+            }
+        }
+        this.adjustmentType = AdjustmentType;
     }
 
     // Add additional constructors and methods as needed
@@ -58,11 +130,11 @@ public class Punch {
         this.originalTimeStamp = originalTimeStamp;
     }
 
-    public long getAdjustedTimeStamp() {
+    public LocalDateTime getAdjustedTimeStamp() {
         return adjustedTimeStamp;
     }
 
-    public void setAdjustedTimeStamp(long adjustedTimeStamp) {
+    public void setAdjustedTimeStamp(LocalDateTime adjustedTimeStamp) {
         this.adjustedTimeStamp = adjustedTimeStamp;
     }
 
@@ -72,5 +144,25 @@ public class Punch {
 
     public void setAdjustmentType(PunchAdjustmentType adjustmentType) {
         this.adjustmentType = adjustmentType;
+    }
+
+    public int getPunchTypeId() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean getType() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public Object getOriginaltimestamp() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public static class PunchType {
+
+        public static boolean CLOCK_IN;
+
+        public PunchType() {
+        }
     }
 }
