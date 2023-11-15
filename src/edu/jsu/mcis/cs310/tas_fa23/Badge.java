d7f2e891524b49027883fbb829ffd0fe76efedc3
Jalen Momon <Jmomon@stu.jsu.edu>
2023-11-15 12:47:23 -0600

    Updated Badge.java to version 2

--- src/edu/jsu/mcis/cs310/tas_fa23/Badge.java
+++ src/edu/jsu/mcis/cs310/tas_fa23/Badge.java
@@ -1,11 +1,20 @@
 package edu.jsu.mcis.cs310.tas_fa23;
 
+import java.util.zip.CRC32;
+
 public class Badge {
 
     private final String id, description;
 
+    // Existing constructor
     public Badge(String id, String description) {
         this.id = id;
+        this.description = description;
+    }
+
+    // New constructor for creating Badge from description and generating ID
+    public Badge(String description) {
+        this.id = generateBadgeID(description);
         this.description = description;
     }
 
@@ -17,16 +26,20 @@ public class Badge {
         return description;
     }
 
-    @Override
-    public String toString() {
-
-        StringBuilder s = new StringBuilder();
-
-        s.append('#').append(id).append(' ');
-        s.append('(').append(description).append(')');
-
-        return s.toString();
-
+    // Helper method to generate a badge ID using CRC-32 checksum
+    private String generateBadgeID(String description) {
+        CRC32 crc32 = new CRC32();
+        crc32.update(description.getBytes());
+        long checksum = crc32.getValue();
+        return String.format("#%08X", checksum);
     }
 
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append('#').append(id).append(' ');
+        s.append('(').append(description).append(')');
+        return s.toString();
+    }
 }
+
