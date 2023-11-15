459d9bef12659e81fb02499c48bb5f431be272eb
Jalen Momon <Jmomon@stu.jsu.edu>
2023-11-15 12:47:36 -0600

    Updated BadgeDao.java to version 2

--- src/edu/jsu/mcis/cs310/tas_fa23/dao/BadgeDAO.java
+++ src/edu/jsu/mcis/cs310/tas_fa23/dao/BadgeDAO.java
@@ -1,59 +1,48 @@
 package edu.jsu.mcis.cs310.tas_fa23.dao;
 
 import edu.jsu.mcis.cs310.tas_fa23.Badge;
+
 import java.sql.*;
 
 public class BadgeDAO {
 
     private static final String QUERY_FIND = "SELECT * FROM badge WHERE id = ?";
+    private static final String QUERY_INSERT = "INSERT INTO badge (id, description) VALUES (?, ?)";
+    private static final String QUERY_DELETE = "DELETE FROM badge WHERE id = ?";
 
     private final DAOFactory daoFactory;
 
     BadgeDAO(DAOFactory daoFactory) {
-
         this.daoFactory = daoFactory;
-
     }
 
     public Badge find(String id) {
-
         Badge badge = null;
-
         PreparedStatement ps = null;
         ResultSet rs = null;
 
         try {
-
             Connection conn = daoFactory.getConnection();
 
             if (conn.isValid(0)) {
-
                 ps = conn.prepareStatement(QUERY_FIND);
                 ps.setString(1, id);
 
                 boolean hasresults = ps.execute();
 
                 if (hasresults) {
-
                     rs = ps.getResultSet();
 
                     while (rs.next()) {
-
                         String description = rs.getString("description");
                         badge = new Badge(id, description);
-
                     }
-
                 }
-
             }
 
         } catch (SQLException e) {
-
             throw new DAOException(e.getMessage());
-
         } finally {
-
             if (rs != null) {
                 try {
                     rs.close();
@@ -68,11 +57,33 @@ public class BadgeDAO {
                     throw new DAOException(e.getMessage());
                 }
             }
-
         }
 
         return badge;
-
     }
 
+    // New method to create a badge in the database
+    public boolean create(Badge badge) {
+        try (Connection conn = daoFactory.getConnection();
+             PreparedStatement ps = conn.prepareStatement(QUERY_INSERT)) {
+            ps.setString(1, badge.getId());
+            ps.setString(2, badge.getDescription());
+            int rowsAffected = ps.executeUpdate();
+            return rowsAffected == 1;
+        } catch (SQLException e) {
+            throw new DAOException(e.getMessage());
+        }
+    }
+
+    // New method to delete a badge from the database
+    public boolean delete(String badgeId) {
+        try (Connection conn = daoFactory.getConnection();
+             PreparedStatement ps = conn.prepareStatement(QUERY_DELETE)) {
+            ps.setString(1, badgeId);
+            int rowsAffected = ps.executeUpdate();
+            return rowsAffected == 1;
+        } catch (SQLException e) {
+            throw new DAOException(e.getMessage());
+        }
+    }
 }
