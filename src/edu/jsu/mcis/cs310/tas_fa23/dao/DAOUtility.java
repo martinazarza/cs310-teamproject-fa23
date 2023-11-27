28e598e639ab3e6c2676b9f9c9d70b38fd7a369e
Jalen Momon <Jmomon@stu.jsu.edu>
2023-11-27 11:10:39 -0600

    Completed Time Accrued method

--- src/edu/jsu/mcis/cs310/tas_fa23/dao/DAOUtility.java
+++ src/edu/jsu/mcis/cs310/tas_fa23/dao/DAOUtility.java
@@ -1,10 +1,45 @@
+package edu.jsu.mcis.cs310.tas_fa23.dao;
+
 import java.util.ArrayList;
 import java.util.HashMap;
-import com.github.cliftonlabs.json_simple.Jsoner;
+import com.github.cliftonlabs.json_simple.*;
+
+import edu.jsu.mcis.cs310.tas_fa23.Punch;
 
 public class DAOUtility {
 
-    public static String getPunchListAsJSON(ArrayList<Punch> dailypunchlist) {
+    public static int calculateTotalMinutes(ArrayList<Punch> dailypunchlist, Shift shift) {
+        int totalMinutes = 0;
+        boolean clockedIn = false;
+        Punch lastPunch = null;
+
+        for (Punch punch : dailypunchlist) {
+            if (punch.getPunchtypeid() == Punch.CLOCK_IN) {
+                clockedIn = true;
+            } else if (punch.getPunchtypeid() == Punch.CLOCK_OUT || punch.getPunchtypeid() == Punch.TIME_OUT) {
+                if (clockedIn) {
+                    int minutesWorked = calculateMinutesWorked(lastPunch, punch, shift);
+                    totalMinutes += minutesWorked;
+                    clockedIn = false;
+                }
+            }
+
+            lastPunch = punch;
+        }
+
+        return totalMinutes;
+    }
+
+    private static int calculateMinutesWorked(Punch startPunch, Punch endPunch, Shift shift) {
+        int minutesWorked = endPunch.getMinutesSinceMidnight() - startPunch.getMinutesSinceMidnight();
+
+        if (shift.getLunchDeduct() && minutesWorked >= shift.getLunchThreshold()) {
+            minutesWorked -= shift.getLunchDuration();
+        }
+
+        return minutesWorked;
+    }
+ public static String getPunchListAsJSON(ArrayList<Punch> dailypunchlist) {
         ArrayList<HashMap<String, String>> jsonData = new ArrayList<>();
 
         for (Punch punch : dailypunchlist) {
@@ -12,9 +47,9 @@ public class DAOUtility {
 
             punchData.put("id", String.valueOf(punch.getId()));
             punchData.put("badgeid", punch.getBadgeid());
-            punchData.put("terminalid", String.valueOf(punch.getTerminalid()));
-            punchData.put("punchtype", punch.getPunchtype().toString());
-            punchData.put("adjustmenttype", punch.getAdjustmenttype().toString());
+            punchData.put("terminalid", punch.getTerminalid());
+            punchData.put("punchtype", punch.getPunchType().toString());
+            punchData.put("adjustmenttype", punch.getAdjustmentType().toString());
             punchData.put("originaltimestamp", punch.printOriginal());
             punchData.put("adjustedtimestamp", punch.printAdjusted());
 
