8280611c15e018d4c4e8120c6a9d845d8216923b
Xavier Bausley <xbausley@stu.jsu.edu>
2023-11-29 11:54:57 -0600

    Almost completed AbsenteeismDAO

--- src/edu/jsu/mcis/cs310/tas_fa23/dao/AbsenteeismDAO.java
+++ src/edu/jsu/mcis/cs310/tas_fa23/dao/AbsenteeismDAO.java
@@ -6,6 +6,7 @@ package edu.jsu.mcis.cs310.tas_fa23.dao;
 
 import edu.jsu.mcis.cs310.tas_fa23.Absenteeism;
 import edu.jsu.mcis.cs310.tas_fa23.Employee;
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -24,8 +25,54 @@ public class AbsenteeismDAO {
         this.daoFactory = daoFactory;
     }
     public Absenteeism find(Employee employee, LocalDate localDate){
+        Absenteeism absenteeism = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
         
-     
+        try{
+            Connection connection = daoFactory.getConnection();
+            String query = "SELECT * FROM absenteeism WHERE employeeid, payperiod, percentage) VALUES (?, ?, ?)";
+            statement = connection.prepareStatement(query);
+            statement.setInt(1, employee.getId());
+            statement.setDate(2, java.sql.Date.valueOf(localDate));
+            resultSet = statement.executeQuery();
+            if(resultSet.next()){
+                absenteeism = extractAbsenteeismFromResultSet(resultSet);
+            }
+        } catch(SQLException e){
+            e.printStackTrace();
+        }finally{
+            
+        }
+        return absenteeism;
     }
-    
+    public void create(Absenteeism absenteeism){
+        PreparedStatement statement = null;
+        
+        try{
+            Connection connection = daoFactory.getConnection();
+            String query = "REPLACE INTO absenteeism (employeeid, payperiod, percentage) VALUES (?, ?, ?)";
+            statement = connection.prepareStatement(query);
+            statement.setInt(1, absenteeism.getEmployee().getId());
+            statement.setDate(2, java.sql.Date.valueOf(absenteeism.getLocalDate()));
+            statement.setBigDecimal(3, absenteeism.getAbsentPercent());
+            statement.executeUpdate();
+        }catch (SQLException e){
+            e.printStackTrace();
+        } finally{
+            
+        }
+    }
+    private Absenteeism extractAbsenteeismFromResultSet(ResultSet resultSet) throws SQLException{
+        int employeeId = resultSet.getInt("employeeid");
+        LocalDate localDate = resultSet.getDate("payperiod").toLocalDate();
+        BigDecimal percentage = resultSet.getBigDecimal("percentage");
+        
+        Employee employee = fetchEmployeeDetails(employeeId);
+        return new Absenteeism(employee, localDate, percentage);
+    }
+    private Employee fetchEmployeeDetails(int employeeId){
+        EmployeeDAO employeeDAO = daoFactory.getEmployeeDAO();
+        return employeeDAO.find(employeeId);
+    }
 }
